21452787e4aff786b77ae816bcc49d92db20511f
kelly_maass <kmaass77627@gmail.com>
2025-08-15 13:12:35 -0500

    new

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -9,12 +9,20 @@ def call(env)
   req = Rack::Request.new(env)
 
    if req.path.match(/recipes/) && req.get?
-     resp.write Recipe.all.to_json
+    newrecipes = []
+    Recipe.all.each do |recipe|
+      recipe.adjusted_title
+      recipe.adjusted_ingredients_text
+      # recipe.adjusted_directions_text
+      newrecipes << recipe
+    end
+    resp.write newrecipes.to_json
+    #  resp.write Recipe.all.to_json
 
   elsif req.path.match(/recipes/) && req.post?
     data = JSON.parse req.body.read
-  #  recipe = Recipe.create(name: data["name"], ingredients: data["ingredients"], directions: data["directions"], image: data["image"], category: data["category"])
-  recipe = Recipe.create(name: data["name"], ingredients: data["ingredients"], directions: data["directions"], image: data["image"])
+   recipe = Recipe.create(name: data["name"], ingredients: data["ingredients"], directions: data["directions"], image: data["image"], category_id: data["category_id"])
+  #  recipe = Recipe.create(name: data["name"], ingredients: data["ingredients"], directions: data["directions"], category: data["category"], image: data["image"])
    resp.write(recipe.to_json)
 
   elsif req.path.match(/categories/) && req.get?
--- backend/app/models/category.rb
+++ backend/app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ActiveRecord::Base
 
 has_many :recipes
 validates :name, uniqueness: true
-
+# validates :name, presence: true
 
 
 
